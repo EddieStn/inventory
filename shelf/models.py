a6f265f18b207deb870274c75c63f0b2d8ffb478
EddieStn <eduard.eduard25@yahoo.com>
2022-12-29 17:03:49 +0000

    Create Item model

--- shelf/models.py
+++ shelf/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class Item(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False)
+    description = models.TextField(blank=True)
+    quantity = models.PositiveIntegerField(default=0)
+    crated_on = models.DateTimeField(auto_now_add=True)
+    timestamp = models.DateTimeField(auto_now=True)
+    image = CloudinaryField('image', default='placeholder')
+
+    class Meta:
+        ordering = ['-timestamp']
+
+    def __str__(self):
+        return self.name
