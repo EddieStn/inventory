fc77ab651e3fdc1a2cf30d896f89fdd5794ae9d9
EddieStn <eduard.eduard25@yahoo.com>
2022-12-29 17:34:41 +0000

    Register the Item model and create the custom ItemAdmin class to admin.py

--- shelf/admin.py
+++ shelf/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Item
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(Item)
+class ItemAdmin(SummernoteModelAdmin):
+
+    summernote_fields = ('description')
+    list_display = ('name', 'timestamp')
+    search_fields = ['name', 'description']
