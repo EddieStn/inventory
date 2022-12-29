171784b8d35c206860f8e8481f4f1c68fa28efa6
EddieStn <eduard.eduard25@yahoo.com>
2022-12-29 18:31:20 +0000

    Create generic view for the Item model

--- shelf/views.py
+++ shelf/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Item
 
-# Create your views here.
+
+class ItemList(generic.ListView):
+    model = Item
+    queryset = Item.objects.order_by('-timestamp')
+    template_name = 'index.html'
+    paginate_by = 10
